0237c96c41523d43f4b96365c2c62c04afe22f59
Mdlalose4life <blessing1059@gmail.com>
2023-08-08 14:30:45 +0200

    The path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,14 @@ class Auth:
             returns :
                 Booleon
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """args:
