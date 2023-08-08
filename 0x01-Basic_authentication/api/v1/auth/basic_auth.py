69caf3ed9dc2f3e664bed7325cba98ba557a9614
Mdlalose4life <blessing1059@gmail.com>
2023-08-08 23:28:09 +0200

    basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module for basic authentication
 """
-
+import base64
+import binascii
 from models.user import User
 from .auth import Auth
 
@@ -10,4 +11,19 @@ class BasicAuth(Auth):
     """
     Basic Authantication class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        extracts the base64 part of the Authorization header.
+         Args:
+            authorization_header (str): The Authorization header string.
+        Returns:
+            str: The Base64 part of the Authorization header, or None if the
+            header is invalid.
+        """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split('Basic ')[1].strip()
