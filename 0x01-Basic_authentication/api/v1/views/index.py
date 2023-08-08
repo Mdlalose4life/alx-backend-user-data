9e19b4a463c4208af19e491a116671a636636d98
Mdlalose4life <blessing1059@gmail.com>
2023-08-08 08:16:37 +0200

    The requirements

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """
-for testing the new 402 error hander.
-    1. route must be GET /api/v1/unauthorized
-    2. the endpoint must raise 401 error, use abort.
+Index view Module
 """
 from flask import abort, jsonify
+from api.v1.views import app_views
 
-
-@app.route("/api/v1/unauthorized")
-def get_ Unauthorized():
-    abort(401)
+@app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
+def get_ Unauthorized() -> str:
+    """
+    for testing the new 402 error hander.
+        1. route must be GET /api/v1/unauthorized
+        2. the endpoint must raise 401 error, use abort.
+    """
+    abort(401, description="Unauthorized")
