75bff7a6de72ccb1c8291f57912d530917c0fa78
Mdlalose4life <blessing1059@gmail.com>
2023-08-10 06:50:15 +0200

    push all

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+""" Class that managesthe API authantication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    description
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """agrs:
+                path
+                excluded_paths
+            returns :
+                Booleon
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_paths in excluded_paths:
+            if excluded_paths.startswith(path):
+                return False
+            elif path.startswith(excluded_paths):
+                return False
+            elif excluded_paths[-1] == "*":
+                if path.startswith(excluded_paths[-1]):
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """args:
+                request
+            returns:
+                string
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return None
+
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """args:
+                request
+            returns:
+                TypeVar
+        """
+        return None
