3a103897e9042c0034013b71213a4ac8b40b94fb
Mdlalose4life <blessing1059@gmail.com>
2023-08-11 05:19:12 +0200

    Create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,25 @@
 """Module for basic authentication
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     Sesion Auth Class that inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create sesssion Method creates a Sesion ID for user ID
+        """
+        # if user id is a string
+        if type(user_id) is not str:
+            return None
+        else:
+            # Generate the Session ID for the user
+            sessison_id = str(uuid4())
+            self.user_id_by_session_id[sessison_id] = user_id
+            # Return Session ID
+            return sessison_id
