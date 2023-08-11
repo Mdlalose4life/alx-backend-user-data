f3a3db1499c7e9e39fb15580b5b6f26185e1bd33
Mdlalose4life <blessing1059@gmail.com>
2023-08-11 05:05:25 +0200

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for basic authentication
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Sesion Auth Class that inherits from Auth
+    """
+    pass
