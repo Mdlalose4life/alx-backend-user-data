2c1ebe130e880388996248554386780b39f342ea
Mdlalose4life <blessing1059@gmail.com>
2023-08-16 12:54:46 +0200

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,7 +36,8 @@ class DB:
                 email
                 hash_password
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+
+        return new_user
