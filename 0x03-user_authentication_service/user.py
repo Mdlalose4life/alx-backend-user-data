aa9b19ab993a666bdffaab3daa8ad3d59f007e42
Mdlalose4life <blessing1059@gmail.com>
2023-08-16 12:36:35 +0200

    users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,15 +7,16 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     """
-    This method creates the SQLAlchemy model named User for 
+    This method creates the SQLAlchemy model named User for
     database table named users
     """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
-    reset_token= Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
